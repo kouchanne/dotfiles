c8ce004e5814880677f25fd7bd2f3be59e39bf17
kouki <hinoatarusakamichi@gmail.com>
2021-02-28 00:50:01 +0900

    anyenvを githubから直接initする方式にした

--- fish/config.fish
+++ fish/config.fish
@@ -1,2 +1,7 @@
-set PATH /opt/homebrew/bin $PATH
 export LSCOLORS=gxfxcxdxbxegedabagacad
+
+## M1 MAC cannot be installed traditional file path
+set PATH /opt/homebrew/bin $PATH
+
+set -x PATH $HOME/.anyenv/bin $PATH
+eval (anyenv init - | source)
