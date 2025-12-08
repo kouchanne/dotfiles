1b6a699c8555be1dc34c4564b13b4486f7524dc0
Koki Okawa <hinoatarusakamichi@gmail.com>
2025-12-08 16:46:32 +0900

    fix: add docker comopse alias and git alias

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -21,3 +21,35 @@ if command -q eza
     abbr -a ll eza --icons -lhag --time-style long-iso
     abbr -a lt eza --icons --tree
 end
+
+# docker compose alias
+function docker
+    # 引数があり、かつ最初の引数が "compose" の場合
+    if test (count $argv) -gt 0; and test "$argv[1]" = "compose"
+        # "docker-compose" コマンドを実行（残りの引数を渡す）
+        command docker-compose $argv[2..-1]
+    else
+        # それ以外は通常の "docker" コマンドを実行
+        command docker $argv
+    end
+end
+
+# git alias
+abbr -a g git
+abbr -a gs git status
+abbr -a ga git add .
+abbr -a gc git commit -v
+abbr -a gcm git commit -m 
+abbr -a gp git push
+abbr -a gl git pull
+
+abbr -a gb git branch
+abbr -a gco git checkout
+abbr -a gcb git checkout -b
+
+abbr -a gd git diff
+abbr -a glog "git log --oneline --graph --decorate"
+
+abbr -a gca git commit --amend
+abbr -a gsta git stash
+abbr -a gstp git stash pop
